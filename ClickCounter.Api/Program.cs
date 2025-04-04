7943cf118c952c02a9e5eff5ba50217186d87aa8
Jahmali Mcknight <jahmali@rampinteractive.onmicrosoft.com>
2025-04-04 02:15:51 -0600

    Configure Authentication Scheme

--- ClickCounter.Api/Program.cs
+++ ClickCounter.Api/Program.cs
@@ -1,5 +1,9 @@
+using System.Text;
 using ClickCounter.Application;
 using ClickCounter.Infrastructure;
+using ClickCounter.Shared.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using Serilog;
 
@@ -13,6 +17,9 @@ try {
     Log.Information("Starting Click Counter API");
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+    JwtSettings jwtSettings = new();
+    builder.Configuration.GetSection(nameof(JwtSettings)).Bind(jwtSettings);
+    builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(nameof(JwtSettings)));
     builder.Services.AddControllers();
     builder.Services.AddOpenApi();
     builder.Services.AddApplication();
@@ -24,14 +31,31 @@ try {
             return httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
         });
 
+    builder.Services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateIssuer = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtSettings.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key)),
+            ValidateLifetime = true
+        };
+    });
+    builder.Services.AddAuthorization();
+    
     WebApplication app = builder.Build();
-
     if (app.Environment.IsDevelopment()) {
         app.MapOpenApi();
         app.MapScalarApiReference();
     }
 
     app.UseHttpsRedirection();
+    app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
     app.Run();
