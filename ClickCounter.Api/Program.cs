0300b3f171049cb0337db553554dfa3b600370c2
Jahmali Mcknight <jahmali@rampinteractive.onmicrosoft.com>
2025-04-04 01:48:15 -0600

    Configure Serilog

--- ClickCounter.Api/Program.cs
+++ ClickCounter.Api/Program.cs
@@ -1,27 +1,42 @@
 using ClickCounter.Application;
 using ClickCounter.Infrastructure;
 using Scalar.AspNetCore;
+using Serilog;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped(typeof(CancellationToken), serviceProvider => {
-    IHttpContextAccessor httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
-    return httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
-});
+try {
+    Log.Information("Starting Click Counter API");
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-WebApplication app = builder.Build();
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddScoped(typeof(CancellationToken),
+        serviceProvider => {
+            IHttpContextAccessor httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            return httpContextAccessor.HttpContext?.RequestAborted ?? CancellationToken.None;
+        });
 
-if (app.Environment.IsDevelopment()) {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    WebApplication app = builder.Build();
+
+    if (app.Environment.IsDevelopment()) {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+} catch (Exception ex) {
+    Log.Fatal(ex, "Unhandled exception");
+} finally {
+    Log.CloseAndFlush();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
